4d810c406b345edba603b0d7f27bc8f7e1641d95
andrict05 <tomislav.andric247@gmail.com>
2023-05-12 22:47:32 +0200

    added mobile navigation

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const teamEl = document.querySelector('.section--team');
 const heroEl = document.querySelector('.hero__container');
 
 window.addEventListener('scroll', () => {
-	const height = navigationEl.getBoundingClientRect().height;
-	if (teamEl.getBoundingClientRect().y <= height) {
-		navigationEl.classList.add('sticky');
-	} else {
-		navigationEl.classList.remove('sticky');
-	}
+  const height = navigationEl.getBoundingClientRect().height;
+  if (teamEl.getBoundingClientRect().y <= height) {
+    navigationEl.classList.add('sticky');
+  } else {
+    navigationEl.classList.remove('sticky');
+  }
+});
+
+document.querySelector('.mobile-nav').addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+document.querySelectorAll('.nav__li').forEach((el) => {
+  el.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  });
 });

c6552f5d67c1af6cf9c2782b5b25e85a76849f78
jakipro121 <71126982+jakipro121@users.noreply.github.com>
2023-05-20 15:51:07 +0200

    karte

---
+++ project/js/script.js
@@ -0,0 +1,15 @@
+cards = document.getElementsByClassName("card");
+console.log(cards.length);
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("card-active");
+    if (this.classList.contains("card-active")) {
+      this.classList.remove("card-active");
+    } else {
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" card-active", "");
+      }
+      this.classList.add("card-active");
+    }
+  });
+}
